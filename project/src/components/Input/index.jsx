616cfa8130cd1c77521f44c25b2417702389ad37
vinimedeiros13 <vinimedeiros7@outlook.com>
2021-06-28 21:37:09 -0300

    removendo o projeto das aulas anteriores

--- project/src/components/Input/index.jsx
+++
@@ -1,9 +0,0 @@
-import './styles.css';
-import P from 'prop-types';
-
-export const Input = ({ searchValue, handleChange }) => <input onChange={handleChange} value={searchValue} placeholder="Faça sua busca" type="search" />;
-
-Input.propTypes = {
-  searchValue: P.string.isRequired,
-  handleChange: P.func.isRequired,
-};
--- project/src/components/PostCard/mock.js
+++
@@ -1,6 +0,0 @@
-export const PostCardPropsMock = {
-  title: 'algum title',
-  body: 'algum body',
-  id: 1,
-  cover: 'img/img.png',
-};
