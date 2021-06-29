f7ffa0fb0fcea4575f31c45a3984fbd34fa4bbf8
vinimedeiros13 <vinimedeiros7@outlook.com>
2021-06-28 23:19:20 -0300

    criando o projeto e colocando o eslint

--- project/.eslintrc.js
+++ project/.eslintrc.js
@@ -3,12 +3,12 @@ module.exports = {
     browser: true,
     es2021: true,
     jest: true,
+    node: true,
   },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
   ],
   parserOptions: {
     ecmaFeatures: {
