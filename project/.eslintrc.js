2f080a64185f6874a6060c4f7731badd2899066c
vinimedeiros13 <vinimedeiros7@outlook.com>
2021-07-04 13:16:27 -0300

    adionando o hook useRef

--- project/.eslintrc.js
+++ project/.eslintrc.js
@@ -26,9 +26,17 @@ module.exports = {
   },
   rules: {
     'react/react-in-jsx-scope': 'off',
-    'import/no-extraneous-dependencies': 'off',
     'react/jsx-filename-extension': 'off',
+    'react/jsx-one-expression-per-line': 'off',
     'react/require-default-props': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-noninteractive-element-interactions': 'off',
+    'jsx-one-expression-per-line': 'off',
+    'no-noninteractive-element-interactions': 'off',
     'no-shadow': 'off',
+    'no-plusplus': 'off',
+    'no-console': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'click-events-have-key-events': 'off',
   },
 };
--- project/src/App.js
+++ project/src/App.js
@@ -1,37 +1,27 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable no-plusplus */
+import {
+  useEffect, useState, useRef,
+} from 'react';
 import './App.css';
-import { useEffect, useMemo, useState } from 'react';
-import P from 'prop-types';
-
-const Posts = ({ posts }) => (
-  <div key={posts.id} className="post">
-    <h1>{posts.title}</h1>
-    <p>{posts.body}</p>
-  </div>
-);
-
-Posts.propTypes = {
-  posts: P.shape({ // Sempre que for objeto utilizar o .shape
-    id: P.number,
-    title: P.string,
-    body: P.string,
-  }),
-};
 
 function App() {
-  const [posts, setPosts] = useState([]);
   const [value, setValue] = useState('');
+  const contador = useRef(0);
 
-  // ComponentDidMount
   useEffect(() => {
-    setTimeout(() => {
-      fetch('https://jsonplaceholder.typicode.com/posts')
-        .then((r) => r.json())
-        .then((r) => setPosts(r));
-    }, 5000);
-  }, []);
+    contador.current++;
+  });
+
+  const handleClick = (value) => {
+    setValue(value);
+  };
 
   return (
     <div className="App">
+      <h2>
+        Renderizou: {contador.current} x
+      </h2>
       <p>
         <input
           type="search"
@@ -39,14 +29,7 @@ function App() {
           onChange={(e) => setValue(e.target.value)}
         />
       </p>
-
-      {useMemo(() => {
-        posts.map((posts) => (
-          <Posts key={posts.id} posts={posts} />
-        ));
-      }, [posts])}
-
-      { }
+      <button type="button" onClick={handleClick}>Render</button>
     </div>
   );
 }
