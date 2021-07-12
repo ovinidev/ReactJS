f1301e11c0efa4709145cfa041825c55a94993e5
vinimedeiros13 <vinimedeiros7@outlook.com>
2021-07-11 23:17:38 -0300

    conteudos de aula

--- project/src/App.js
+++ project/src/App.js
@@ -1,12 +1,41 @@
+/* eslint-disable default-case */
+import { useReducer } from 'react';
 import './App.css';
-import { AppContext } from './contexts/AppContext/index';
-import { Div } from './components/Div';
+
+export const globalState = { // Estado inicial
+  title: 'Titulo do contexto',
+  body: 'body do contexto',
+  counter: 0,
+};
+
+// action recebe o valor do type:
+const reducer = (state, action) => {
+  switch (action.type) { // dispatch sendo utilizado
+    case 'muda':
+      console.log('Chamou muda');
+      return { ...state, title: 'Mudou' };
+    case 'inverter':
+      console.log('Chamou inverter');
+      const {title} = state;
+      return { ...state, title: title.split('').reverse().join('') };
+  }
+
+  return { ...state }; // Retornando o estado novo, obrigatoriamente
+};
 
 function App() {
+  // dispatch irá disparar ações, no caso irá disparar a função reducer
+  const [state, dispatch] = useReducer(reducer, globalState);
+  const { counter, title, body } = state;
+
   return (
-    <AppContext>
-      <Div />
-    </AppContext>
+    <div>
+      <h1>
+        {title} {counter}
+      </h1>
+      <button onClick={() => dispatch({ type: 'muda' })}>Click</button>
+      <button onClick={() => dispatch({ type: 'inverter' })}>Inverte</button>
+    </div>
   );
 }
 
--- project/src/contexts/AppContext/data.js
+++
@@ -1,5 +0,0 @@
-export const globalState = { // Estado inicial
-  title: 'Titulo do contexto',
-  body: 'body do contexto',
-  counter: 0,
-};
