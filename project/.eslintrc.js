8a0fd72a63d83dfdbb1c3b696241fe7d48448699
vinimedeiros13 <vinimedeiros7@outlook.com>
2021-07-22 18:42:40 -0300

    adicionando um hook

--- project/.eslintrc.js
+++ project/.eslintrc.js
@@ -44,6 +44,7 @@ module.exports = {
     'react/prop-types': 'off',
     'jsx-one-expression-per-line': 'off',
     'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
     'jsx-a11y/no-noninteractive-element-interactions': 'off',
     'jsx-a11y/control-has-associated-label': 'off',
     'jsx-a11y/anchor-is-valid': 'off',
@@ -68,6 +69,6 @@ module.exports = {
     'max-len': 'off',
     'arrow-body-style': 'off',
     'default-case': 'off',
-    'jsx-a11y/no-static-element-interactions': 'off',
+    'prefer-const': 'off',
   },
 };
